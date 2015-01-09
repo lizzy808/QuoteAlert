e4581d73ff2c4208e6a23334e4caf1c143dc8cd2
Scott Martin <smartin@Scotts-MacBook-Pro-10.local>
2015-01-09 13:17:22 -0800

    Created a subclass for the final tutorial view which allows for a done button to dismiss the view controller. Removed the segues from storyboard since we are handling in code.

---
+++ Quote Alert/TutorialViewController4.h
@@ -0,0 +1,14 @@
+//
+//  TutorialViewController4.h
+//  Quote Alert
+//
+//  Created by Scott Martin on 1/9/15.
+//  Copyright (c) 2015 Elizabeth Choy. All rights reserved.
+//
+//  This is a simple subclass allowing for a done button to dismiss the view controller
+
+#import "MSPageViewControllerPage.h"
+
+@interface TutorialViewController4 : MSPageViewControllerPage
+
+@end
